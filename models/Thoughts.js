72950fcff15e51bc4aa86b223a6bd4689a7ba2cb
bret jensen <jensenbret13@gmail.com>
2025-04-03 19:32:07 -0700

    models

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -5,7 +5,15 @@ const thoughtSchema = new Schema(
         thoughtText: {
             type: String,
             required: true,
+            minlength: 1,
+            maxlength: 280
+        }
+        createdAt: {
             
         }
     }
 )
+
+const Thoughts = model("Thoughts", thoughtSchema);
+
+export default Thoughts;
